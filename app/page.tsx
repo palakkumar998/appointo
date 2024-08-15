44e428154eed680682c6908f42c9789221a330f5
Palak kumar <116839013+palakkumar998@users.noreply.github.com>
2024-08-16 01:07:09 +0530

    patient form, zod, react from implemented

--- app/page.tsx
+++ app/page.tsx
@@ -1,12 +1,34 @@
 import { Button } from "@/components/ui/button";
+import PatientForm from "@/components/ui/forms/PatientForm";
 import Image from "next/image";
+import Link from "next/link";
 
 export default function Home() {
   return (
 
-    <div>
-      <h1 className="text-4xl underline text-white" >Home</h1>
-      <Button >click here</Button>
+    <div className="flex h-screen max-h-screen">
+
+      {/* //todo : OTP varifiction | passkey model */}
+
+      <section className=" remove-scrollbar container my-auto">
+        <div className="sub-container max-w-[496px]" >
+          <Image src="/assets/icons/logo-full.svg"
+            height={1000}
+            width={1000}
+            alt="patient name"
+            className="mb-12 h-10 w-fit" />
+        </div>
+
+        <PatientForm />
+
+        <div className="text-10-regular mt-5 justify-between flex " >
+          <p className="justify-items-end text-dark-600 xl:text-left" >© 2024 Copyright | Appointo</p>
+          <Link href="/?admin = true" className="text-green-300">Admin</Link>
+        </div>
+      </section>
+      {/* here is the image section */}
+      <Image src="/assets/images/onboarding-img.png" height={1000} width={1000} alt="patient" className="side-img max-w-[50%]" />
+
     </div>
   );
 }
---
+++ components/ui/forms/PatientForm.tsx
@@ -0,0 +1,67 @@
+"use client"
+
+import { zodResolver } from "@hookform/resolvers/zod"
+import { useForm } from "react-hook-form"
+import { z } from "zod"
+import { Button } from "@/components/ui/button"
+import {
+    Form,
+    FormControl,
+    FormDescription,
+    FormField,
+    FormItem,
+    FormLabel,
+    FormMessage,
+} from "@/components/ui/form"
+import { Input } from "@/components/ui/input"
+
+
+const formSchema = z.object({
+    username: z.string().min(2, {
+        message: "Username must be at least 2 characters.",
+    }),
+})
+
+const PatientForm = () => {
+    // 1. Define your form.
+    const form = useForm<z.infer<typeof formSchema>>({
+        resolver: zodResolver(formSchema),
+        defaultValues: {
+            username: "",
+        },
+    })
+
+    // 2. Define a submit handler.
+    function onSubmit(values: z.infer<typeof formSchema>) {
+        console.log(values)
+    }
+    return (
+        <Form {...form}>
+            <form onSubmit={form.handleSubmit(onSubmit)} className="space-y-8">
+                <section className="mb-10 space-y-4">
+                    <h1 className="header" >hi there ! 👋</h1>
+                    <p className="text-dark-700" >Schedule your first Appointement with Appionto</p>
+                </section>
+                <FormField
+                    control={form.control}
+                    name="username"
+                    render={({ field }) => (
+                        <FormItem>
+                            <FormLabel>Username</FormLabel>
+                            <FormControl>
+                                <Input placeholder="shadcn" {...field} />
+                            </FormControl>
+                            <FormDescription>
+                                This is your public display name.
+                            </FormDescription>
+                            <FormMessage />
+                        </FormItem>
+                    )}
+                />
+                <Button type="submit">Submit</Button>
+            </form>
+        </Form>
+    )
+}
+
+export default PatientForm
