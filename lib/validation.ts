98e6505e531aa0918242811afcb05025f63badb1
Palak kumar <116839013+palakkumar998@users.noreply.github.com>
2024-08-20 23:15:12 +0530

    minor change inphone no validation

--- lib/validation.ts
+++ lib/validation.ts
@@ -5,6 +5,6 @@ export const UserFormValidation = z.object({
         .min(2, "Name must be at least 2 characters.")
         .max(50, "Name must be at least 50 characters."),
     email: z.string().email("Invalid email address"),
-    phone: z.string().refine((Phone) => /^\+?[1-9]\d{1,14}$/.test(phone), 'Invalid Phone Number')
+    phone: z.string().refine((phone) => /^\+\d{10,15}$/.test(phone), "Invalid phone number"),
 
 })
